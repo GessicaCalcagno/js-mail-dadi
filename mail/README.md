2eb26aaf894e3156bee64ad8647f8a529389ec8d
GessicaCalcagno <gessicacristinacalcagno@gmail.com>
2024-03-29 17:18:43 +0100

    Add: scaffoldin mail es.

---
+++ mail/README.md
@@ -0,0 +1,15 @@
+> Esercizio
+Chiedi all’utente la sua email,
+controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull’esito del controllo.
+
+###### Svolgimento
+1. [] Chiedere all'utente il suo indirizzo email
+
+*Parte logica:*
+1. [] Creo un Array per indicare chi può accedere.
+2. [] Se l'email non è tra quelle che possono accedere: segna in console = accesso negato.
+ - altrimenti: accesso consentito.
+
+ *Output:*
+ Stampa in pagina il risultato
---
+++ mail/js/script.js
@@ -0,0 +1,40 @@
+// TEST
+//alert('ciao')
+
+//************************************************ */
+// Lo usiamo come controllo alla fine se c'è qualche problema di sintassi! COMMENTALO SE ANCORA STAI LAVORANDO SUL CODICE
+//"use strict";
+//**************************************************** */
+
+//1- Chiedo all'utente la sua email
+const mailUser = prompt("Scrivi il tuo indirizzo email")
+
+//Array
+const userAccess = ["gessica@gmail.com", "gessica@virgilio.it", "gessica@live.it", "pippo@libero.it "]
+
+// Creo una variabile globale 
+let flag = false; // boolean
+
+// Creo ciclo for per entrare nell'array
+for (let i = 0; i < userAccess.length; i++) {
+    //console.log(i, userAccess[i]); --> Mi stampa tutti gli elementi
+
+    if (userAccess === mailUser) {
+        flag = true;
+        console.log("Accesso consentito");
+        // L'istruzione break serve a uscire immediatamente dal ciclo dopo aver trovato un match, senza continuare a iterare sugli elementi successivi.
+        break;
+    } else {
+        console.log("Accesso negato")
+    }
+}
+
+
+//Output
+
+if (flag === true) {
+    document.getElementById("result").innerHTML = "Accesso consentito";
+} else {
+    document.getElementById("result").innerHTML = "Accesso negato";
+}
+
